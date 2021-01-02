fc80f3bccb14eccab7755a35785cbb307789fbdb
TheOmyNomy <theomynomy@gmail.com>
2021-01-02 11:22:21 +1100

    Use NuGet release of OsuMemoryDataProvider and ProcessMemoryDataFinder

--- Klserjht/Properties/AssemblyInfo.cs
+++ Klserjht/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Klserjht")]
-[assembly: AssemblyCopyright("Copyright © TheOmyNomy 2018 - 2019")]
+[assembly: AssemblyCopyright("Copyright © TheOmyNomy 2018 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.12.05.0")]
-[assembly: AssemblyFileVersion("2019.12.05.0")]
+[assembly: AssemblyVersion("2021.01.02.0")]
+[assembly: AssemblyFileVersion("2021.01.02.0")]
