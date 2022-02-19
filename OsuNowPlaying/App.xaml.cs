0f7fde345d985b143a4628210ba57477c68b9dc0
TheOmyNomy <theomynomy@nomy.sh>
2022-02-19 13:17:15 +1100

    Allocate a console for debug configs / builds

--- OsuNowPlaying/App.xaml.cs
+++ OsuNowPlaying/App.xaml.cs
@@ -1,3 +1,28 @@
-﻿namespace OsuNowPlaying;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
 
-public partial class App { }
+namespace OsuNowPlaying;
+
+public partial class App
+{
+#if DEBUG
+	[DllImport("kernel32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static extern bool AllocConsole();
+
+	[DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+	private static extern bool FreeConsole();
+
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		AllocConsole();
+		base.OnStartup(e);
+	}
+
+	protected override void OnExit(ExitEventArgs e)
+	{
+		FreeConsole();
+		base.OnExit(e);
+	}
+#endif
+}
